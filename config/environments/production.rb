88c033c4aaf28df2d0d57942d719a50a074b6326
authentic <galaxy73rambler@yahoo.com>
2012-11-07 16:12:59 -0500

    Newsletter

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,5 +83,5 @@ MassanuttenlionsCom::Application.configure do
   }
 
   config.assets.precompile += Ckeditor.assets
-  config.assets.precompile += %w( print.css )
+  config.assets.precompile += %w( print.css admin.js )
 end
