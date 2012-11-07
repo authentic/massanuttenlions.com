0657cb89fd0b1e908964eaa0bc99ac6e45aa0112
authentic <galaxy73rambler@yahoo.com>
2012-11-07 14:13:57 -0500

    Newsletter

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,6 +37,22 @@ set :scm, "git"
 set :branch, "master"
 set :deploy_via, :remote_cache
 
+
+#############################################################
+#	Assets
+#############################################################
+namespace :deploy do
+  namespace :assets do
+    desc "Precompile assets on local machine and upload them to the server."
+    task :precompile, roles: :web, except: {no_release: true} do
+      run_locally "bundle exec rake assets:precompile"
+      find_servers_for_task(current_task).each do |server|
+        run_locally "rsync -vr --exclude='.DS_Store' public/assets/precompile #{:user}@#{server.host}:#{shared_path}/"
+      end
+    end
+  end
+end
+
 #############################################################
 #	Passenger
 #############################################################
